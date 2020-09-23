7e1f78d94364dffd9afa0f17c7cdc384ed84fa0c
hongyue-qiu <994059641@qq.com>
2020-09-23 22:42:25 +0800

    [qiuhongyue] finished exercise

--- src/exercise-4/components/User.js
+++ src/exercise-4/components/User.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import NotMatch from "./NotMatch";
 
-const User = () => {
-  return (
-    <div>
-      User profile page.
-    </div>
-  );
+const User = (props) => {
+    const id = props.match.params.user;
+
+    const reg = /^[0-9]*$/g;
+
+    if (reg.test(id)) {
+        return <div>User profile page.</div>;
+    }
+
+    return <NotMatch/>;
 };
 
 export default User;
